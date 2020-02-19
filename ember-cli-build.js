b743eca9bdeadd478216b78882bab0542505ac39
Roger F. Del Nobre <desenv4@labplus.com.br>
2020-02-19 08:26:18 -0300

    Ticket_id: #87292 - Adiciona ember-bootstrap ao projeto

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,10 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
-    // Add options here
+    'ember-bootstrap': {
+      'bootstrapVersion': 4,
+      'importBootstrapCSS': true
+    }
   });
 
   app.import('node_modules/jquery-mask-plugin/dist/jquery.mask.min.js')
