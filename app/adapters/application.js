0fff5d772b284f69b8e08e536ef61719dc72e3f8
Roger F. Del Nobre <desenv4@labplus.com.br>
2020-03-13 09:34:04 -0300

    Ticket_id: #87959 - Aciciona serializer

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,5 +1,6 @@
 import JSONAPIAdapter from '@ember-data/adapter/json-api';
 
 export default class ApplicationAdapter extends JSONAPIAdapter {
+  host = 'localhost:3010';
   namespace = 'api/v1';
 }
--- app/routes/marcas.js
+++ app/routes/marcas.js
@@ -1,8 +1,8 @@
 import Route from '@ember/routing/route';
 
 export default class MarcasRoute extends Route {
-  model() {
-    console.log('o model acabou de execurar!');
-    return 'Hello Ember!';
-  }
+  // model() {
+  //   console.log('o model acabou de execurar!');
+  //   return 'Hello Ember!';
+  // }
 }
