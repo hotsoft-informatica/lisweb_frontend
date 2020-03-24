c6a1575263fada1171c01f6d50c8b66073cac43a
Roger F. Del Nobre <desenv4@labplus.com.br>
2020-03-24 17:25:48 -0300

    Ticket_id: #88461 - Ajusta problema do render template de marcas

--- app/models/marca.js
+++ app/models/marca.js
@@ -2,7 +2,7 @@ import Model, { attr } from '@ember-data/model';
 
 export default class MarcaModel extends Model {
   @attr('string') nome;
-  @attr('text') descricao;
+  @attr('string') descricao;
   @attr('number') laboratorio_id;
   @attr('number') version_id;
   @attr('boolean', { defaultValue: false }) deleted;
--- app/routes/marcas.js
+++ app/routes/marcas.js
@@ -10,6 +10,6 @@ export default class MarcasRoute extends Route {
   // }
 
   model() {
-    this.store.findAll('marca');
+    return this.store.findAll('marca');
   }
 }
