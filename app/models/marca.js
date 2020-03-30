26d80870f33022f0c7a8fb1469e93acf1ac504d5
Roger F. Del Nobre <desenv4@labplus.com.br>
2020-03-30 14:16:51 -0300

    Ticketj_id #88461 - Ajusta action create

--- app/models/marca.js
+++ app/models/marca.js
@@ -1,10 +1,8 @@
-import Model, { attr } from '@ember-data/model';
+import Model, { attr, belongsTo } from '@ember-data/model';
 
 export default class MarcaModel extends Model {
   @attr('string') nome;
   @attr('string') descricao;
-  @attr('number') laboratorio_id;
-  @attr('number') version_id;
-  @attr('boolean', { defaultValue: false }) deleted;
-  @attr('date', { defaultValue() { return new Date(); } }) created_at;
+  // @attr('number') laboratorio_id;
+  @belongsTo('laboratorio') laboratorio;
 }
--- app/routes/marcas.js
+++ app/routes/marcas.js
@@ -22,15 +22,30 @@ export default class MarcasRoute extends Route {
   // }
 
   model() {
-    var marca = this.store.createRecord('marca', {
+    // https://guides.emberjs.com/release/models/relationships/
+    // https://emberigniter.com/saving-models-relationships-json-api/
+
+    let marca = this.store.createRecord('marca', {
       "nome": "TESTE001",
       "descricao": "ESTE É O TESTE CRIACAO DE TESTE001",
-      "laboratorio_id": '02',
-      "version_id": '007',
-      "deleted": false
+      laboratorio: this.store.findRecord('laboratorio', 1)
     });
-    marca.save();
-    this.store.push(marca);
+
+    function myFunction(item) {
+      console.table(item);
+    }
+
+    function failure(reason) {
+      reason.errors.forEach(myFunction);
+    }
+
+    marca.save().catch(
+      failure
+    ).then(
+      console.info()
+    );
+
+    // this.store.push(marca);
     return this.store.findAll('marca');
   }
 
