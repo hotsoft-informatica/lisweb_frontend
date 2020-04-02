4a2e97589db4973a2eb09c1617614785118ad575
Roger F. Del Nobre <desenv4@labplus.com.br>
2020-04-02 11:36:14 -0300

    Ticket_id: #88803 - Create de marcas

--- app/routes/marcas.js
+++ app/routes/marcas.js
@@ -3,35 +3,19 @@ import Route from '@ember/routing/route';
 
 export default class MarcasRoute extends Route {
 
-  // model() {
-  //   return this.store.createRecord('marca', {
-  //     "nome": "TESTE001",
-  //     "descricao": "ESTE É O TESTE CRIACAO DE TESTE001",
-  //     "laboratorio_id": '02',
-  //     "version_id": '007',
-  //     "deleted": false
-  //   }).save().then(() => {
-  //       return this.store.findAll('marca');
-  //   });
-  // }
-
   model() {
     // https://guides.emberjs.com/release/models/relationships/
     // https://emberigniter.com/saving-models-relationships-json-api/
 
-    let Lab = this.store.peekRecord('laboratorio', 1);
+    let lab = this. store.findRecord('laboratorio', 1);
 
     let marca = this.store.createRecord('marca', {
-      nome: 'TESTE005',
-      descricao: 'ESTE É O TESTE CRIACAO DE TESTE005',
-      laboratorio: Lab
+      nome: 'TESTE006',
+      descricao: 'ESTE É O TESTE CRIACAO DE TESTE006',
+      laboratorio: lab
     });
 
-    marca.save().catch(
-      failure
-    ).then(
-      console.info()
-    );
+    marca.save();
 
     function myFunction(item) {
       console.table(item);
