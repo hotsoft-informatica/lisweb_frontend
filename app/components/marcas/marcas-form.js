06fd98323bbed2082eadf2256fb2dd9250b09d14
Wellington Torrejais da Silva <wtds.trabalho@gmail.com>
2020-05-25 12:16:45 -0300

    ember-cli-update codemods
    Signed-off-by: Wellington Torrejais da Silva <wtds.trabalho@gmail.com>

--- app/components/marcas/marcas-form.js
+++ app/components/marcas/marcas-form.js
@@ -1,7 +1,7 @@
 
 import Component from '@glimmer/component';
 import { action } from "@ember/object";
-import {inject as service} from '@ember/service';
+import { inject as service } from '@ember/service';
 import { isEmpty } from '@ember/utils';
 
 export default class MarcasFormComponent extends Component{
