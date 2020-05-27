2158d3114a60f670d4ef11c81db876547e954bb3
Roger F. Del Nobre <desenv4@labplus.com.br>
2020-05-27 14:40:00 -0300

    Ticket_id: #lisweb_ember_downgrade - Redirect action e uso de clousure

--- app/components/marcas/marcas-form.js
+++ app/components/marcas/marcas-form.js
@@ -8,13 +8,13 @@ export default class MarcasFormComponent extends Component{
   @service router;
 
   @action
-  saveMarca(marca,redirectTo) {
+  saveMarca(marca, redirectTo) {
     self = this;
     marca.save().then( function(){
       if (!isEmpty(redirectTo)){
       // https://api.emberjs.com/ember/release/classes/RouterService
       //self.router.transitionTo(redirectTo);
-      self.sendAction('redirectTo', marca);
+      self.redirectTo();
     }});
   }
 }
--- app/routes/marcas/new.js
+++ app/routes/marcas/new.js
@@ -5,10 +5,11 @@ export default Route.extend({
   model() {
     return this.store.createRecord('marca');
   },
+
   actions: {
     redirectTo: function(){
       console.log("Passou aqui");
-      this.router.transitionTo(redirectTo);
+      this.transitionTo("/marcas");
     }
   }
 });
