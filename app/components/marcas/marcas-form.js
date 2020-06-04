14f4a6b4e241c8e977784a77e2297eb80a27de39
Roger F. Del Nobre <desenv4@labplus.com.br>
2020-06-04 14:55:16 -0300

    sp315 #lisweb_redirect_component - Redirect component url atriburo redirectTo

--- app/components/marcas/marcas-form.js
+++ app/components/marcas/marcas-form.js
@@ -1,17 +1,23 @@
 
 import Component from '@ember/component';
+import { action } from "@ember/object";
+import {inject as service} from '@ember/service';
+import { isEmpty } from '@ember/utils';
 
 
-export default Component.extend({
+export default class MarcasFormComponent extends Component{
   //https://guides.emberjs.com/v3.3.0/components/triggering-changes-with-actions/
   //https://github.com/ember-cli/eslint-plugin-ember/blob/master/docs/rules/closure-actions.md
 
-  actions: {
-    saveMarca(marca) {
-      let selfthis = this;
-      marca.save().then( function() {
-        selfthis.sendAction('redirectTo');
-      });
-    }
+  @service router;
+
+  @action
+  saveMarca(marca, redirectTo) {
+    let selfthis = this;
+    marca.save().then( function() {
+      if (!isEmpty(redirectTo)){
+        selfthis.router.transitionTo(redirectTo);
+      }
+    });
   }
-});
+}
