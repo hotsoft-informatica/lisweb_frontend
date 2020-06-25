caaa5e99b187e9ac17090859eace39a662815826
Wellington Torrejais da Silva <wtds.trabalho@gmail.com>
2020-06-25 17:20:15 -0300

    Movendo load de dados pro componente, marcas index
    De modo que fique mais independente da rota, facilitando reuso.
    
    TODO: Fazer com o form ,  new e edit de Marcas.

--- app/components/marcas/marcas-index.js
+++ app/components/marcas/marcas-index.js
@@ -1,14 +1,26 @@
-import Component from '@glimmer/component';
 import { action } from '@ember/object';
-// TODO: Converter para Glimmer
-// TODO: Transformar em classe nativa
+import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
+import Component from '@glimmer/component';
 
 export default class MarcasIndexComponent extends Component {
-  // TODO: Usar decorator @action
+  @service store;
+  @tracked errors;
+  @tracked loading='Carregando...';
+  @tracked marcas;
+
+  constructor(owner, args) {
+    super(owner, args);
+    this.store.findAll('marca').then( (marcas) => {
+      this.marcas = marcas;
+    }, (errors) => {
+      this.loading = 'Falha no carregamento!';
+      this.errors = errors;
+    });
+  }
+
   @action
   deleteMarca(marca) {
-    // TODO:Expected 'this' to be used by class method 'deleteMarca'.
-    // TODO: eslintclass-methods-use-this
     marca.destroyRecord();
   }
 }
--- app/routes/marcas/index.js
+++ app/routes/marcas/index.js
@@ -1,15 +1,3 @@
 import Route from '@ember/routing/route';
-import { inject as service } from '@ember/service';
 
-// TODO: Native JS classes should be used instead of classic classes
-// TODO: eslintember/no-classic-classes
-export default class MarcasIndexRoute extends Route {
-  @service store;
-
-  async model() {
-    return this.store.query('marca', {
-      limit: 10,
-      offset: 0
-    });
-  }
-}
+export default class MarcasIndexRoute extends Route {}
