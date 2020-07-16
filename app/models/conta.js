e24b6d53dadaf28abe1de0dc324bc40e4ae52140
Roger F. Del Nobre <desenv4@labplus.com.br>
2020-07-16 14:36:13 -0300

    Cria template e ajustes

--- app/models/conta.js
+++ app/models/conta.js
@@ -3,10 +3,10 @@ import Model, { attr, belongsTo } from '@ember-data/model';
 export default class ContaModel extends Model {
   @attr('string') tipo;
   @attr('string') nome;
-  @attr('number') saldo;
-  @attr('number') version;
-  @attr('number') version_id;
-  @attr('number') laboratory_domain_id;
-  @attr('boolean') deleted;
+  // @attr('number') saldo;
+  // @attr('number') version;
+  // @attr('number') version_id;
+  // @attr('number') laboratory_domain_id;
+  // @attr('boolean') deleted;
   @belongsTo('laboratorio') laboratorio;
 }
