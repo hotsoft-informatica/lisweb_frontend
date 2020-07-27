4e6a0597b877587dfa5e413e5aed71dc9f0d45ae
Roger F. Del Nobre <desenv4@labplus.com.br>
2020-07-27 16:24:01 -0300

    sp318 - Crud e novo paradigma centro custo

--- app/models/tipo-produto.js
+++ app/models/tipo-produto.js
@@ -4,9 +4,9 @@ export default class TipoProdutoModel extends Model {
   @attr('string') mnemonico;
   @attr('string') nome;
   @attr('string') descricao;
+  @belongsTo('laboratorio') laboratorio;
   // @attr('number') unidade_medida_saida_id;
   // @attr('number') estoque_minimo;
   // @attr('number') estoque_ideal;
-  // @belongsTo('laboratorio') laboratorio;
   // @belongsTo('produtos') produto;
 }
