8757dca86289c98e4aa0e8b7c6f1a0ee521bdb8e
Roger F. Del Nobre <desenv4@labplus.com.br>
2020-07-29 15:58:26 -0300

    sp318 - Crud template component categoria produtos

--- app/routes/categoria-produtos/edit.js
+++ app/routes/categoria-produtos/edit.js
@@ -2,6 +2,8 @@ import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 
 export default class CategoriaProdutosEditRoute extends Route {
+  @service store;
+
   async model(params) {
     return this.store.findRecord('categoria_produto', params.categoria_produto_id);
   }
