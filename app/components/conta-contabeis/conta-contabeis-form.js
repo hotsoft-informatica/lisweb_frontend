d9abe42adcc864d7398226a1f1880da11cb276f3
Roger F. Del Nobre <desenv4@labplus.com.br>
2020-08-05 08:22:01 -0300

    sp319 - CRUD template components router plano de contas

---
+++ app/components/conta-contabeis/conta-contabeis-form.js
@@ -0,0 +1,6 @@
+import { tracked } from '@glimmer/tracking';
+import DefaultComponent from '../default/default';
+
+export default class ContaContabeisFormComponent extends DefaultComponent {
+  @tracked redirectTo = '/conta_contabeis';
+}
--- app/routes/conta-contabeis/new.js
+++ app/routes/conta-contabeis/new.js
@@ -1,8 +1,9 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
-// import CentroCusto from 'lisweb-frontend/models/centro-custo';
 
-export default class ContaContabeisNewRoute extends Route {
+export default class  ContaContabeisNewRoute extends Route {
+  @service store;
+
   async model() {
     return this.store.createRecord('conta_contabil');
   }
