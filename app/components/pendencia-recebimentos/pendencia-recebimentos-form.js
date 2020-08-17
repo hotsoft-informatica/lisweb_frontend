dd7a3136b7a1e6ec08a4e6c4f93c23daf9364958
Roger F. Del Nobre <desenv4@labplus.com.br>
2020-08-17 11:20:01 -0300

    sp320 Crud Template Router Components - pendencia recebimento

---
+++ app/components/pendencia-recebimentos/pendencia-recebimentos-form.js
@@ -0,0 +1,6 @@
+import { tracked } from '@glimmer/tracking';
+import DefaultComponent from '../default/default';
+
+export default class PendenciaRecebimentosFormComponent extends DefaultComponent {
+  @tracked redirectTo = '/pendencia_recebimentos';
+}
