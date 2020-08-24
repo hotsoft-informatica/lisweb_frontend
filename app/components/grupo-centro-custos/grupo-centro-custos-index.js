f455ef2ae954aa523b58fb9c0bd53ef75b59b99e
Roger F. Del Nobre <desenv4@labplus.com.br>
2020-08-24 15:23:09 -0300

    sp320 Paginacao template grupo_cetro_custo

--- app/components/grupo-centro-custos/grupo-centro-custos-index.js
+++ app/components/grupo-centro-custos/grupo-centro-custos-index.js
@@ -1,27 +1,6 @@
-import { action } from '@ember/object';
-import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
-import Component from '@glimmer/component';
+import DefaultComponent from '../default/default';
 
-export default class GrupoCentroCustosIndexComponent extends Component {
-  // TODO: Usar decorator @action
-  @service store;
-  @tracked errors;
-  @tracked loading='Carregando...';
-  @tracked model;
-
-  constructor(owner, args) {
-    super(owner, args);
-    this.store.findAll('grupo_centro_custo').then( (model) => {
-      this.model = model;
-    }, (errors) => {
-      this.loading = 'Falha no carregamento!';
-      this.errors = errors;
-    });
-  }
-
-  @action
-  delete(model) {
-    model.destroyRecord();
-  }
+export default class GrupoCentroCustosIndexComponent extends DefaultComponent {
+  @tracked modelString = 'grupo_centro_custo';
 }
