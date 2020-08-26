cf29b60c4c2215aa6b6d501c93437b83f02f6f3a
Roger F. Del Nobre <desenv4@labplus.com.br>
2020-08-26 14:43:10 -0300

    sp321 - Model default component e paginacao Movimentacao

--- app/components/atendimento-requisicoes/atendimento-requisicoes-index.js
+++ app/components/atendimento-requisicoes/atendimento-requisicoes-index.js
@@ -1,13 +1,6 @@
+import { tracked } from '@glimmer/tracking';
 import DefaultComponent from '../default/default';
 
 export default class AtendimentoRequisicoesIndexComponent extends DefaultComponent {
-  constructor(owner, args) {
-    super(owner, args);
-    this.store.findAll('atendimento_requisicao').then( (model) => {
-      this.model = model;
-    }, (errors) => {
-      this.loading = 'Falha no carregamento!';
-      this.errors = errors;
-    });
-  }
+  @tracked modelString = 'atendimento_requisicao';
 }
--- app/components/baixa-produtos/baixa-produtos-index.js
+++ app/components/baixa-produtos/baixa-produtos-index.js
@@ -1,13 +1,6 @@
+import { tracked } from '@glimmer/tracking';
 import DefaultComponent from '../default/default';
 
 export default class BaixaProdutosIndexComponent extends DefaultComponent {
-  constructor(owner, args) {
-    super(owner, args);
-    this.store.findAll('baixa_produto').then( (model) => {
-      this.model = model;
-    }, (errors) => {
-      this.loading = 'Falha no carregamento!';
-      this.errors = errors;
-    });
-  }
+  @tracked modelString = 'baixa_produto';
 }
