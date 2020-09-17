24aa77b19515c58f430eebace5a2436bfc998d9c
Roger F. Del Nobre <desenv4@labplus.com.br>
2020-09-17 09:16:02 -0300

    sp321 Relacionamento grupo_centro_custo

--- app/models/grupo-centro-custo.js
+++ app/models/grupo-centro-custo.js
@@ -4,5 +4,5 @@ export default class GrupoCentroCustoModel extends Model {
   @attr('string') nome;
   // @attr('boolean') deleted;
   // @attr('number') changed_by_lab_id;
-  // @belongsTo('laboratorio') laboratorio;
+  @belongsTo('laboratorio') laboratorio;
 }
