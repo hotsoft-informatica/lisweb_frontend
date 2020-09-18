bbf79183ccedef02d642b723c02f28beeb6a90fa
Roger F. Del Nobre <desenv4@labplus.com.br>
2020-09-18 15:05:08 -0300

    sp321 Relacionamento unidade_medida

--- app/components/unidade-medidas/unidade-medidas-form.js
+++ app/components/unidade-medidas/unidade-medidas-form.js
@@ -1,6 +1,13 @@
+import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 import DefaultComponent from '../default/default';
 
 export default class UnidadeMedidasFormComponent extends DefaultComponent {
   @tracked redirectTo = '/unidade_medidas';
+  @tracked modelString = 'unidade_medida';
+
+  @action
+  save(model, ...event) {
+    super.save(model, ...event);
+  }
 }
