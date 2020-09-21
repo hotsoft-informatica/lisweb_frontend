3747712636f59ad452492995d7ad4e9092e206be
Roger F. Del Nobre <desenv4@labplus.com.br>
2020-09-21 09:43:29 -0300

    sp322 Relacionamento conta_contabil

--- app/components/conta-contabeis/conta-contabeis-form.js
+++ app/components/conta-contabeis/conta-contabeis-form.js
@@ -1,6 +1,13 @@
+import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 import DefaultComponent from '../default/default';
 
 export default class ContaContabeisFormComponent extends DefaultComponent {
   @tracked redirectTo = '/conta_contabeis';
+  @tracked modelString = 'conta_contabil';
+
+  @action
+  save(model, ...event) {
+    super.save(model, ...event);
+  }
 }
