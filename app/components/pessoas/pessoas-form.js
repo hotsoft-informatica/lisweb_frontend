b4adba22226ec2437a48d66a09d2dacbdba7b01f
Roger F. Del Nobre <desenv4@labplus.com.br>
2020-09-23 09:26:55 -0300

    sp322 Relacionamentos

--- app/components/pessoas/pessoas-form.js
+++ app/components/pessoas/pessoas-form.js
@@ -1,20 +1,13 @@
-import Component from '@glimmer/component';
 import { action } from '@ember/object';
-import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
+import DefaultComponent from '../default/default';
 
-// https://guides.emberjs.com/v3.3.0/components/triggering-changes-with-actions/
-// https://github.com/ember-cli/eslint-plugin-ember/blob/master/docs/rules/closure-actions.md
-// https://api.emberjs.com/ember/release/classes/RouterService
-export default class PessoasFormComponent extends Component {
-  // TODO: 'service' is defined but never used.eslintno-unused-vars
-  @service router;
+export default class PessoasFormComponent extends DefaultComponent {
+  @tracked redirectTo = '/pessoas';
+  @tracked modelString = 'pessoa';
 
   @action
-  save(pessoa, redirectTo = 'redirectTo') {
-    // TODO: Resolver sem esse selfthis
-    let selfthis = this;
-    pessoa.save().then(function () {
-      selfthis.router.transitionTo(redirectTo);
-    });
+  save(model, ...event) {
+    super.save(model, ...event);
   }
 }
--- app/components/tipo-pessoas/tipo-pessoas-form.js
+++ app/components/tipo-pessoas/tipo-pessoas-form.js
@@ -1,6 +1,13 @@
+import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 import DefaultComponent from '../default/default';
 
 export default class TipoPessoasFormComponent extends DefaultComponent {
   @tracked redirectTo = '/tipo_pessoas';
+  @tracked modelString = 'tipo_pessoa';
+
+  @action
+  save(model, ...event) {
+    super.save(model, ...event);
+  }
 }
