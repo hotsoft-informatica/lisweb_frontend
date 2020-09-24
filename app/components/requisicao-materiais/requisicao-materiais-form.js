91c71f3dad017ea9196f84f545e78c0283d2d2bc
Roger F. Del Nobre <desenv4@labplus.com.br>
2020-09-24 16:07:13 -0300

    sp322 - requisicao_materias

--- app/components/requisicao-materiais/requisicao-materiais-form.js
+++ app/components/requisicao-materiais/requisicao-materiais-form.js
@@ -1,6 +1,16 @@
 import { tracked } from '@glimmer/tracking';
-import DefaultComponent from '../default/default';
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 
-export default class RequisicaoMateriaisFormComponent extends DefaultComponent {
+export default class RequisicaoMateriaisFormComponent extends Component {
+  @service router;
   @tracked redirectTo = '/requisicao_materiais';
+
+  @action
+  save(model, ...event) {
+    model.save().then( () => {
+      this.router.transitionTo(this.redirectTo);
+    });
+  }
 }
