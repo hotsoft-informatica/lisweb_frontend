1073a2ad21cc1412501bd42726146973045c47b6
Roger F. Del Nobre <desenv4@labplus.com.br>
2020-09-24 17:35:59 -0300

    sp322 Relacionamento movientacoes

--- app/components/atendimento-requisicoes/atendimento-requisicoes-form.js
+++ app/components/atendimento-requisicoes/atendimento-requisicoes-form.js
@@ -1,6 +1,13 @@
+import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 import DefaultComponent from '../default/default';
 
 export default class AtendimentoRequisicoesFormComponent extends DefaultComponent {
   @tracked redirectTo = '/atendimento_requisicoes';
+  @tracked modelString = 'atendimento_requisicao';
+
+  @action
+  save(model, ...event) {
+    super.save(model, ...event);
+  }
 }
--- app/components/pendencia-recebimentos/pendencia-recebimentos-form.js
+++ app/components/pendencia-recebimentos/pendencia-recebimentos-form.js
@@ -1,6 +1,9 @@
+import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 import DefaultComponent from '../default/default';
 
 export default class PendenciaRecebimentosFormComponent extends DefaultComponent {
   @tracked redirectTo = '/pendencia_recebimentos';
+  @tracked modelString = 'pendencia_recebimento';
+
 }
