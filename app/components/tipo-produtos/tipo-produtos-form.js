75f8379fb9bde24641cf68d6757d40b4d68f4734
Roger F. Del Nobre <desenv4@labplus.com.br>
2020-11-04 14:17:18 -0300

    sp324 EmberX-Select template component

--- app/components/tipo-produtos/tipo-produtos-form.js
+++ app/components/tipo-produtos/tipo-produtos-form.js
@@ -2,10 +2,13 @@ import { A } from '@ember/array';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 import DefaultComponent from '../default/default';
+import { inject as service } from '@ember/service';
 
 export default class TipoProdutosFormComponent extends DefaultComponent {
+  @service store;
   @tracked redirectTo = '/tipo_produtos';
   @tracked modelString = 'tipo_produto';
+  @tracked categoria_produtos = this.store.findAll('categoria_produto');
 
   @tracked modelStrings = A(['categoria_produto', 'unidade_medida']);
 
