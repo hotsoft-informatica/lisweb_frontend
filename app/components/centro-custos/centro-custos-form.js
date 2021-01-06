61ad013a0405b0535d1a71d7e901fced83f57a9a
Roger F. Del Nobre <desenv4@labplus.com.br>
2021-01-06 16:59:34 -0300

    Issue #113
    Not rendered: frontile forms ChangesetForm

--- app/components/centro-custos/centro-custos-form.js
+++ app/components/centro-custos/centro-custos-form.js
@@ -1,10 +1,37 @@
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 import DefaultComponent from '../default/default';
+import { Changeset } from 'ember-changeset';
+import lookupValidator from 'ember-changeset-validations';
+import validatePresence from 'ember-changeset-validations/validators/presence';
 
 export default class CentroCustosFormComponent extends DefaultComponent {
   @tracked redirectTo = '/centro_custos';
   @tracked modelString = 'centro_custo';
+
+  myFormModel = {
+    name: {
+      first: 'Ron',
+      last: 'Swanson'
+    },
+    email: 'ron@pawnee.in.gov',
+    hometown: 'Pawnee, IN',
+    preferences: ['Lagavulin', 'Steak', 'Bacon', 'No govt.']
+  };
+
+  validations = {
+    myValue: validatePresence(true)
+  };
+
+  myModel = {
+    myValue: ''
+  };
+
+  myChangeset = new Changeset(
+    this.myModel,
+    lookupValidator(this.validations),
+    this.validations
+  );
 
   @action
   save(model, ...event) {
