98ee9119c0ccf284fdb9e381ec8821aa1081cddf
Roger F. Del Nobre <desenv4@labplus.com.br>
2021-01-18 16:15:05 -0300

    Teste ember-powe-select array simples

--- app/components/tipo-pessoas/tipo-pessoas-form.js
+++ app/components/tipo-pessoas/tipo-pessoas-form.js
@@ -6,6 +6,14 @@ export default class TipoPessoasFormComponent extends DefaultComponent {
   @tracked redirectTo = '/tipo_pessoas';
   @tracked modelString = 'tipo_pessoa';
 
+  tipospessoa = ['Administrador', 'Laboratorio', 'Usuario', 'Paciente']
+  tipo = 'Selecione'
+
+  @action
+  selecTipo (pessoa) {
+    this.set('tipo', pessoa);
+  }
+
   @action
   save(model, ...event) {
     super.save(model, ...event);
