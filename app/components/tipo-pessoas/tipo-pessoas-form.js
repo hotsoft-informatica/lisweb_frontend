837a336d87028f11b887ff3a83012dfe4ec251af
Roger F. Del Nobre <desenv4@labplus.com.br>
2021-01-27 15:07:23 -0300

    sp328 Ember changeset teste

--- app/components/tipo-pessoas/tipo-pessoas-form.js
+++ app/components/tipo-pessoas/tipo-pessoas-form.js
@@ -1,6 +1,7 @@
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 import DefaultComponent from '../default/default';
+import { Changeset } from 'ember-changeset';
 
 export default class TipoPessoasFormComponent extends DefaultComponent {
   @tracked redirectTo = '/tipo_pessoas';
@@ -9,13 +10,35 @@ export default class TipoPessoasFormComponent extends DefaultComponent {
   tipospessoa = ['Administrador', 'Laboratorio', 'Usuario', 'Paciente']
   tipo = 'Selecione'
 
-  @action
-  selecTipo (pessoa) {
-    this.set('tipo', pessoa);
+  init(...args) {
+    super.init(...args)
+
+    //let validator = this.validate;
+    this.changeset = Changeset(this.tipospessoa);
   }
+  // @action
+  // selecTipo (pessoa) {
+  //   this.set('tipo', pessoa);
+  // }
 
   @action
   save(model, ...event) {
     super.save(model, ...event);
   }
+
+  @action
+  submit(changeset) {
+    return changeset.save();
+  }
+
+  @action
+  rollback(changeset) {
+    return changeset.rollback();
+  }
+
+  // @action
+  // validate({ key, newValue, oldValue, changes, content }) {
+  //   // lookup a validator function on your favorite validation library
+  //   // and return a Boolean
+  // }
 }
